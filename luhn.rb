b6dce6e4e6d24f219f78fd88c530b2a2a91d4d8b
BackStreetSoy <jordanedwardobey@gmail.com>
2018-09-24 16:16:36 -0400

    added comments, refactored

--- luhn.rb
+++ luhn.rb
@@ -4,23 +4,21 @@ def validate_number(num)
     original_numbers = num.to_s.split("").map! do |x| x.to_i end
     position = original_numbers.length - 2 
 
-
+    #starting at the second most right digit, multiply every other value by 2
      until position < 0
-        puts position
          original_numbers[position] = 2 * (original_numbers[position])
          position -= 2 
      end 
 
+    #if a value is greater than 9, split it into two seperate values and sum them together (ex. 12 -> 1 + 2)
     original_numbers.map! do |x|
-        if x > 9 
-            x.to_s.split("").map! {|y| y.to_i }.reduce(&:+)
-        else 
-            x
-        end 
+        x > 9 ? (x.to_s.split("").map! {|y| y.to_i }.reduce(&:+)) : x
     end 
 
     original_numbers.flatten!
 
     original_numbers.reduce(&:+)%10 == 0 ? (puts "\u001b[32mvalid number") : (puts "\u001b[31mnumber invalid" +" please try again.")
-    
+
 end 
+
+validate_number(4552250100232342)
